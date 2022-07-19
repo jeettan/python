6201ee4d62185efe9b0261a27ddf4911927eafb9
jeettan <85212575+jeettan@users.noreply.github.com>
2022-07-19 14:04:33 +0700

    Add files via upload

---
+++ randomforest.py
@@ -0,0 +1,27 @@
+#Import scikit-learn dataset library
+from sklearn import datasets
+
+#Load dataset
+iris = datasets.load_iris()
+
+# print the label species(setosa, versicolor,virginica)
+print(iris.target_names)
+
+# print the names of the four features
+print(iris.feature_names)
+
+# print the iris data (top 5 records)
+print(iris.data[0:5])
+
+# print the iris labels (0:setosa, 1:versicolor, 2:virginica)
+print(iris.target)
+
+import pandas as pd
+data=pd.DataFrame({
+    'sepal length':iris.data[:,0],
+    'sepal width':iris.data[:,1],
+    'petal length':iris.data[:,2],
+    'petal width':iris.data[:,3],
+    'species':iris.target
+})
+data.head()
